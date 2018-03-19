49fbc3c311bdf12b5def82a4aedd0234b48efa21
Pape BADIANE <pape.badiane@openio.io>
2018-03-19 18:18:47 +0100

    add a dashboard for each service

--- script.sh
+++ script.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
-serveur='127.0.0.0.1'
-baseCommande ='swift -A http://'+$serveur+':6007/auth/v1.0/ -U demo:demo -K DEMO_PASS '
-stat = $baseCommande+'stat '
-post = $baseCommande+'post '
-delete = $baseCommande+'delete '
-upload = $baseCommande+'upload '
-auth = $baseCommande+'auth '
-list = $baseCommande+'list '
-copy= $baseCommande+'copy '
-download = $baseCommande+'download '
+serveur="127.0.0.1"
+basecommande="swift -A http://$serveur:6007/auth/v1.0/ -U demo:demo -K DEMO_PASS"
+stat="$basecommande stat"
+post="$basecommande post"
+delete="$basecommande delete"
+upload="$basecommande upload"
+auth="$basecommande auth"
+list="$basecommande list"
+copy="$basecommande copy"
+download="$basecommande download"
+
 ### simulation
 
 
-$post+'test'
+
+
+for (( c=1; c<=100; c++ ))
+do
+   #create a container with a random name
+   id=`cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes 3`
+   repo="repo-$id"
+   $post $repo
+
+   # upload file /etc/network/interfaces
+   file="/etc/network/interfaces"
+   $upload $repo $file
+   echo "[UPLOAD] $file --> $repo"
+
+   #upload file /etc/hosts
+   file="/etc/hosts"
+   $upload $repo $file
+   echo "[UPLOAD] $file --> $repo"
+
+   #delete one file  among uploaded
+   $delete $repo $file
+   echo "[DELETE] $file --> $repo"
+   
+done
+
+#print stat, auth and list
+
+$list
+$stat
+$auth
